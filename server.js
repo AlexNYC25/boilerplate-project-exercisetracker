818816aa24d740692a6a91742163dad7b5803c2d
Alexis Montes <montes.alexis25@gmail.com>
2021-06-19 12:28:46 -0400

    set up iniitial route handling

--- server.js
+++ server.js
@@ -9,7 +9,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// pass form data : username, return obj with username and _id
+app.post('/api/users', (req, res) => {
 
+})
+
+// get an arr of all users, each ele is username and _id
+app.get('/api/users', (req, res) => {
+
+})
+
+// pass form data: description, duration, and optionaly date use curr date if not passed, response is user object with fields added
+app.post('/api/users/:_id/exercises', (req, res) => {
+
+})
+
+// get user object with logs array added with each element having the exercise fields
+// has aditition count object that counts how many exercises
+// add parameters from to and limit to the request
+app.get('/api/users/:_id/logs', (req, res) => {
+
+})
 
 
 
