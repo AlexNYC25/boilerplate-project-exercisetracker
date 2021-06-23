8dde9da8ae110cbcd61b456f685ee9e4d5356534
Alexis Montes <montes.alexis25@gmail.com>
2021-06-22 23:55:57 -0400

    created inital post users route

--- server.js
+++ server.js
@@ -1,17 +1,58 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+
 require('dotenv').config()
+
+const userName = process.env.MONGO_USERNAME;
+const password = process.env.MONGO_PASSWORD;
+
+
+const URL = "mongodb+srv://"+userName+":"+password+"@cluster0.vrici.mongodb.net/Mechanical_switches?retryWrites=true&w=majority";
+
+// connect to database
+mongoose.connect(URL, {useNewUrlParser: true, useUnifiedTopology: true})
+  .then((result) => {
+    console.log("connected to database");
+  })
+  .catch((err) => {
+    console.log("There was some sort of error in connecting to the database");
+    
+  })
+
+const users = require('./models/users.js')
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({ extended: false }))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+
+
+let getRandomInt = (min, max) => {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 // pass form data : username, return obj with username and _id
 app.post('/api/users', (req, res) => {
+  let username = req.body.username;
+  let id = getRandomInt(100000, 999999)
+  console.log(username)
 
+  users.create({_id: id, username: username}, (err, small) => {
+    if(err){
+      console.log("error inserting new user");
+      res.send("Error");
+      return;
+    }
+    
+    res.send(small);
+    return;
+
+  })
+
+  res.send("Test")
 })
 
 // get an arr of all users, each ele is username and _id
